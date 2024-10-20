3a76c6dc685c02bf58e10f2f60c66c147ca9b35e
VitaWP6 <vita.petraviciute@iamjunior.lt>
2024-10-21 00:50:21 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,195 @@
+function display1() {
+  if (document.getElementById("answerOne").checked) {
+    document.getElementById("disp1").innerHTML = "Neteisingai";
+    answerSubmit.style.display = "none";
+  } else if (document.getElementById("answerTwo").checked) {
+    document.getElementById("disp1").innerHTML = "Teisingai";
+    answerSubmit.style.display = "none";  
+    disp1.style.color = "green";
+  } else if (document.getElementById("answerThree").checked) {
+    document.getElementById("disp1").innerHTML = "Neteisingai";
+    answerSubmit.style.display = "none";    
+  } else {
+    document.getElementById("disp1").innerHTML = "Nepasirinktas joks variantas";
+  }
+}
+
+function display2() {
+  if (document.getElementById("answerOne-2").checked) {
+    document.getElementById("disp2").innerHTML = "Teisingai";
+    answerSubmit2.style.display = "none";
+    disp2.style.color = "green";
+  } else if (document.getElementById("answerTwo-2").checked) {
+    document.getElementById("disp2").innerHTML = "Neteisingai";
+    answerSubmit2.style.display = "none";    
+  } else if (document.getElementById("answerThree-2").checked) {
+    document.getElementById("disp2").innerHTML = "Neteisingai";
+    answerSubmit2.style.display = "none";    
+  } else {
+    document.getElementById("disp2").innerHTML = "Nepasirinktas joks variantas";
+  }
+}
+
+function display3() {
+  if (document.getElementById("answerOne-3").checked) {
+    document.getElementById("disp3").innerHTML = "Teisingai";
+    answerSubmit3.style.display = "none";
+    disp3.style.color = "green";
+  } else if (document.getElementById("answerTwo-3").checked) {
+    document.getElementById("disp3").innerHTML = "Neteisingai";
+    answerSubmit3.style.display = "none";    
+  } else if (document.getElementById("answerThree-3").checked) {
+    document.getElementById("disp3").innerHTML = "Neteisingai";
+    answerSubmit3.style.display = "none";    
+  } else {
+    document.getElementById("disp3").innerHTML = "Nepasirinktas joks variantas";
+  }
+}
+
+function display4() {
+  if (document.getElementById("answerOne-4").checked) {
+    document.getElementById("disp4").innerHTML = "Neteisingai";
+    answerSubmit4.style.display = "none";
+  } else if (document.getElementById("answerTwo-4").checked) {
+    document.getElementById("disp4").innerHTML = "Neteisingai";
+    answerSubmit4.style.display = "none";    
+  } else if (document.getElementById("answerThree-4").checked) {
+    document.getElementById("disp4").innerHTML = "Teisingai";
+    disp4.style.color = "green";
+    answerSubmit4.style.display = "none";    
+  } else {
+    document.getElementById("disp4").innerHTML = "Nepasirinktas joks variantas";
+  }
+}
+
+function display5() {
+  if (document.getElementById("answerOne-5").checked) {
+    document.getElementById("disp5").innerHTML = "Neteisingai";
+    answerSubmit5.style.display = "none";
+  } else if (document.getElementById("answerTwo-5").checked) {
+    document.getElementById("disp5").innerHTML = "Teisingai";
+    disp5.style.color = "green";
+    answerSubmit5.style.display = "none";    
+  } else if (document.getElementById("answerThree-5").checked) {
+    document.getElementById("disp5").innerHTML = "Neteisingai";
+    answerSubmit5.style.display = "none";    
+  } else {
+    document.getElementById("disp5").innerHTML = "Nepasirinktas joks variantas";
+  }
+}
+
+function display6() {
+  if (document.getElementById("answerOne-6").checked) {
+    document.getElementById("disp6").innerHTML = "Teisingai";
+    answerSubmit6.style.display = "none";
+    disp6.style.color = "green";
+  } else if (document.getElementById("answerTwo-6").checked) {
+    document.getElementById("disp6").innerHTML = "Neteisingai";
+    answerSubmit6.style.display = "none";    
+  } else if (document.getElementById("answerThree-6").checked) {
+    document.getElementById("disp6").innerHTML = "Neteisingai";
+    answerSubmit6.style.display = "none";    
+  } else {
+    document.getElementById("disp6").innerHTML = "Nepasirinktas joks variantas";
+  }
+}
+
+function display7() {
+  if (document.getElementById("answerOne-7").checked) {
+    document.getElementById("disp7").innerHTML = "Neteisingai";
+    answerSubmit7.style.display = "none";
+  } else if (document.getElementById("answerTwo-7").checked) {
+    document.getElementById("disp7").innerHTML = "Neteisingai";
+    answerSubmit7.style.display = "none";    
+  } else if (document.getElementById("answerThree-7").checked) {
+    document.getElementById("disp7").innerHTML = "Teisingai";
+    answerSubmit7.style.display = "none";    
+    disp7.style.color = "green";
+  } else {
+    document.getElementById("disp7").innerHTML = "Nepasirinktas joks variantas";
+  }
+}
+
+function display8() {
+  if (document.getElementById("answerOne-8").checked) {
+    document.getElementById("disp8").innerHTML = "Teisingai";
+    answerSubmit8.style.display = "none";
+    disp8.style.color = "green";
+  } else if (document.getElementById("answerTwo-8").checked) {
+    document.getElementById("disp8").innerHTML = "Neteisingai";
+    answerSubmit8.style.display = "none";    
+  } else if (document.getElementById("answerThree-8").checked) {
+    document.getElementById("disp8").innerHTML = "Neteisingai";
+    answerSubmit8.style.display = "none";    
+  } else {
+    document.getElementById("disp8").innerHTML = "Nepasirinktas joks variantas";
+  }
+}
+
+function display9() {
+  if (document.getElementById("answerOne-9").checked) {
+    document.getElementById("disp9").innerHTML = "Neteisingai";
+    answerSubmit9.style.display = "none";
+  } else if (document.getElementById("answerTwo-9").checked) {
+    document.getElementById("disp9").innerHTML = "Teisingai";
+    answerSubmit9.style.display = "none";    
+    disp9.style.color = "green";
+  } else if (document.getElementById("answerThree-9").checked) {
+    document.getElementById("disp9").innerHTML = "Neteisingai";
+    answerSubmit9.style.display = "none";    
+  } else {
+    document.getElementById("disp9").innerHTML = "Nepasirinktas joks variantas";
+  }
+}
+
+function display10() {
+  if (document.getElementById("answerOne-10").checked) {
+    document.getElementById("disp10").innerHTML = "Neteisingai";
+    answerSubmit10.style.display = "none";
+  } else if (document.getElementById("answerTwo-10").checked) {
+    document.getElementById("disp10").innerHTML = "Teisingai";
+    answerSubmit10.style.display = "none";    
+    disp10.style.color = "green";
+  } else if (document.getElementById("answerThree-10").checked) {
+    document.getElementById("disp10").innerHTML = "Neteisingai";
+    answerSubmit10.style.display = "none";    
+  } else {
+    document.getElementById("disp10").innerHTML = "Nepasirinktas joks variantas";
+  }
+}
+
+function getUserName(Vita) {
+  let personName = document.getElementById("person-name").value;
+  let nameInput = document.getElementById("nameInput");
+  nameInput.textContent = personName;
+  let startTestButton = document.getElementById("startTestButton");
+  startTestButton.addEventListener("click", getUserName, false);
+}
+
+
+let result = 10;
+let text = document.getElementById("resultText");
+
+if (result <= 10 && result >= 9) {
+  text.innerHTML = personName + "You are the best!";
+} else if (result <= 8 && result >= 7) {
+  text.innerHTML = userName + "Very good result!";
+} else if (result <= 6 && result >= 5) {
+  text.innerHTML = "Not bad, but I think you can better!";
+} else if (result <= 4) {
+  text.innerHTML = "Someone need to learn harder!";
+} else {
+  text.innerHTML = "Error!";
+}
+
+
+
+// let number = 10;
+
+// if (number <= 10 && number >= 9) {
+//   console.log("The number is greater than 4");
+// } else if (number < 1) {
+//     console.log("The number is less than 1");
+// } else {
+// 	console.log("The number is 4 or less");
+// }
